ec70a3e1c448f96a9e9ba1f9b10ccac65171b0c3
gimdaeyeon <rlaeodus1306@naver.com>
2023-06-18 23:45:12 +0900

    WIP: 로그인 회원가입 작업중 controller진행중 - 김대연(0618)

--- src/main/java/com/example/happypetsday/controller/user/UserController.java
+++ src/main/java/com/example/happypetsday/controller/user/UserController.java
@@ -1,4 +1,19 @@
 package com.example.happypetsday.controller.user;
 
+import com.example.happypetsday.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+
+
+
+
+
 }
