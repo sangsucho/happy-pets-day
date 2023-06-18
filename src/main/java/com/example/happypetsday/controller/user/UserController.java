04bc830055961ab94ebb1e7226ee9d0abc4f8458
gimdaeyeon <rlaeodus1306@naver.com>
2023-06-19 01:40:42 +0900

    Feat: 간단한 회원가입, 로그인 처리 완료(수정할거 좀 있음) - 김대연(0619)

--- src/main/java/com/example/happypetsday/controller/user/UserController.java
+++ src/main/java/com/example/happypetsday/controller/user/UserController.java
@@ -1,29 +1,83 @@
 package com.example.happypetsday.controller.user;
 
+import com.example.happypetsday.dto.UserDto;
+import com.example.happypetsday.service.user.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/user/*")
+@RequiredArgsConstructor
 public class UserController {
-    @GetMapping("/login")
-    public String login(){
-        return "user/login";
-    }
 
+    private final UserService userService;
+
+    //    회원가입 화면 이동
     @GetMapping("/join")
-    public String join(){
+    public String join() {
         return "user/join";
     }
 
+    //    회원가입 처리
+    @PostMapping("/join")
+    public RedirectView join(UserDto userDto) {
+//        추후에 오류 수정후 삭제
+        userDto.setUserAddress("서울시 강남구");
+        userService.register(userDto);
+        return new RedirectView("/user/login");
+    }
+
+    //    로그인 처리
+    @PostMapping("/login")
+    public RedirectView login(String userId, String userPassword, HttpServletRequest req) {
+        try {
+            UserDto user = userService.findUserNumberAndStatus(userId, userPassword);
+            switch (user.getUserStatus()) {
+//                회원의 상태가 관리자인 경우 관리자번호도 함께 부여
+                case 0:
+                    req.getSession().setAttribute("adminNumber", user.getUserNumber());
+                    break;
+//                회원의 상태가 탈퇴 회원인 경우 일단 회원가입 화면으로 이동
+                case -1:
+                    return new RedirectView("/user/join");
+            }
+            req.getSession().setAttribute("userNumber", user.getUserNumber());
+
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return new RedirectView("/user/login");
+        }
+        return new RedirectView("/");
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "user/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest req) {
+        req.getSession().invalidate(); //세션 초기화
+        return "user/login";
+    }
+
+
+
+
+
     @GetMapping("/findId")
-    public String findId(){
+    public String findId() {
         return "user/findId";
     }
 
     @GetMapping("/findPW")
-    public String findPW(){
+    public String findPW() {
         return "user/findPW";
     }
 
