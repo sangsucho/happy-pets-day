3c65719c8c450753c2a74471b662e28f862f7440
sangsucho <whtkdtn2@naver.com>
2023-06-19 13:40:10 +0900

    Refactor : GetMapping 주소 adoptionInformation에서 list로 변경 0619조상수

--- src/main/java/com/example/happypetsday/controller/adopt/AdoptController.java
+++ src/main/java/com/example/happypetsday/controller/adopt/AdoptController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/adopt/*")
 public class AdoptController {
-    @GetMapping("/adoptionInformation")
+    @GetMapping("/list")
     public String AdoptionInformation(){
         return "AdoptionInformation/AdoptionInformation";
     }
