4fbb53d35910209554ceeb9045a4c5fe08e034ac
C02\Administrator <lkh666666@gmail.com>
2023-06-19 13:32:24 +0900

    Feat : 반려동물 추천, 파일 이름 변경 - 이건희 0619

--- src/main/java/com/example/happypetsday/dto/RecommendFileDto.java
+++ src/main/java/com/example/happypetsday/dto/RecommendFileDto.java
@@ -1,4 +1,16 @@
 package com.example.happypetsday.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@NoArgsConstructor
 public class RecommendFileDto {
+    private Long recommendFileNumber;
+    private Long recommendNumber;
+    private String recommendFileName;
+    private String recommendFileUuid;
+    private String recommendFileUploadPath;
 }
