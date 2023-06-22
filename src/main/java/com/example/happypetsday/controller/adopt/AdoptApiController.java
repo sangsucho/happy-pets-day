eae90e829bc80da5bb266d12b312bf6afefc36de
kwangin <snowball0826@gmail.com>
2023-06-22 18:31:20 +0900

    Docs: json-simple 라이브러리 추가, 입양정보 RestController 생성 - 박광인(0622)

---
+++ src/main/java/com/example/happypetsday/controller/adopt/AdoptApiController.java
@@ -0,0 +1,12 @@
+package com.example.happypetsday.controller.adopt;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class AdoptApiController {
+
+}
