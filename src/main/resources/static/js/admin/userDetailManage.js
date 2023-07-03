2c302aca15b543361421af14fa01c9cbfe01cfaa
Jmsuhhh <min567567@naver.com>
2023-07-03 11:32:36 +0900

    Feat : 탈퇴/제명회원일 시 회원복구 기능 구현 완료(alert창까지 전부) - 서지민 0703

--- src/main/resources/static/js/admin/userDetailManage.js
+++ src/main/resources/static/js/admin/userDetailManage.js
@@ -1,3 +1,4 @@
+//회원 삭제
 $('.btn-delete').on('click',function (){
     let confirmMessage = $(this).data('confirm');
     if (confirm(confirmMessage)) {
@@ -7,7 +8,6 @@ $('.btn-delete').on('click',function (){
     let userNumber = $('.user-number').val();
     window.location.href=`/admin/userDetailManage?userNumber=${userNumber}`
 })
-
 
 function modify() {
     let userStatus = $('.btn-delete').val();
@@ -20,6 +20,40 @@ function modify() {
 
     $.ajax({
         url : `/usersManage/delete`,
+        type : 'patch',
+        data : JSON.stringify(user),
+        contentType : 'application/json; charset=utf-8',
+        success : function(){
+            // console.log("ajax연결 완료")
+
+        },
+        error : function (a,b,c){
+            console.log(c);
+        }
+    });
+}
+
+// 회원 복구
+$('.btn-restore').on('click',function (){
+    let confirmMessage = $(this).data('confirm');
+    if (confirm(confirmMessage)) {
+        modifyRestore();
+        alert('회원이 복구되었습니다');
+    }
+    let userNumber = $('.user-number').val();
+    window.location.href=`/admin/userDetailManage?userNumber=${userNumber}`
+})
+
+function modifyRestore() {
+    let userStatus = $('.btn-restore').val();
+    let userNumber = $('.user-number').val();
+    let user = {
+        userNumber:userNumber,
+        userStatus:userStatus
+    }
+
+    $.ajax({
+        url : `/usersManage/restore`,
         type : 'patch',
         data : JSON.stringify(user),
         contentType : 'application/json; charset=utf-8',
